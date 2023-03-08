2899e1823cba36b69a5a8c373964295cff426222
rcsAcademy <rishtoncsa@gmail.com>
2023-03-08 17:43:17 +0500

    Read data from FireStore get category names and images

--- app/src/main/java/com/example/bottomnavigationbar/HomeFragment.java
+++ app/src/main/java/com/example/bottomnavigationbar/HomeFragment.java
@@ -2,63 +2,91 @@ package com.example.bottomnavigationbar;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+// before
+import com.example.bottomnavigationbar.Adaptor.CategoryAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+// after
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.bottomnavigationbar.Domain.CategoryDomain;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private View categoryView;
+    private ArrayList<CategoryDomain> categoryDomain;
+    private CategoryAdapter categoryAdapter;
+    private RecyclerView recyclerview;
+    FirebaseFirestore db;
+    CollectionReference categories;
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        categoryView = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerview = (RecyclerView) categoryView.findViewById(R.id.recyclerVeiw);
+        // creating our new array list
+        categoryDomain = new ArrayList<>();
+        recyclerview.setHasFixedSize(true);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        categories = db.collection("Category");
+        categoryAdapter = new CategoryAdapter(categoryDomain, getContext());
+        recyclerview.setAdapter(categoryAdapter);
+        loadrecyclerviewData();
+        return categoryView;
+    }
+    private void loadrecyclerviewData() {
+
+        categories.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for (DocumentSnapshot d : list) {
+                                CategoryDomain dataModal = d.toObject(CategoryDomain.class);
+
+                                categoryDomain.add(dataModal);
+                            }
+                            categoryAdapter.notifyDataSetChanged();
+                        } else {
+                            Toast.makeText(getContext(), "No data found in Database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getContext(), "Fail to get the data.", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
